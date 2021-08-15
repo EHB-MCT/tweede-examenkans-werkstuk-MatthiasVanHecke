74da8b09be7f85a974bd0289a11de6ed50e7dc5e
Matthias Van Hecke <matthias.van.hecke@student.ehb.be>
2021-08-15 15:52:48 +0200

    V1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 "use strict";
+
+/*fetch('https://thecrew.cc/news/read.php')
+    .then(response => response.json())
+    .then(data => console.log(data));*/
+
+
+const getArticles = {
+    initFields() {
+        console.log('init');
+        const inputForm = document.getElementById('articleInput').addEventListener('submit', (e) => {
+            e.preventDefault();
+            const search = document.getElementById('articleInputField').value;
+            console.log(search);
+            this.getArticles(search);
+        });
+    },
+    async getArticles(news) {
+        fetch(`https://thecrew.cc/news/read.php`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            let htmlString = `
+            <h2>article</h2> `;
+            document.getElementById('container').innerHTML = htmlString;
+        });
+    }
+};
+
+getArticles.initFields();
